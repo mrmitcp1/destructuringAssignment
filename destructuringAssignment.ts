8d5a36eab31bd593c65bacb83eef0cfdec0600e0
mrmitcp1 <mrmitcp55@gmail.com>
2023-04-03 11:32:06 +0700

    des

---
+++ destructuringAssignment.ts
@@ -0,0 +1,7 @@
+// let names =['cho','meo','lon','ga']
+// // // let [firstName,secondName] = names;
+// // console.log(firstName,secondName)
+// let [firstName,, thirdName]= ['cho','meo','lon','ga']
+// console.log(firstName,thirdName)
+let [firstName,,...lastName]=['cho','meo','lon','ga']
+console.log(firstName,lastName)
